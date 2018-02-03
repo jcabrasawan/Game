597b9d3e2ae9c5f45e6f86c7067954200ed28ac2
Angela Clevenger <angieclever@gmail.com>
2018-02-03 13:03:53 -0600

    finished initial player input

--- player.py
+++ player.py
@@ -8,5 +8,19 @@ class Player:
         print("Inventory:")
         for item in self.invventory:
             print("*"+ str(item))
-        
+        best_weapon = self.most_powerful_weapon()
+        print("Your best weapon is your {}".format(best_weapon))
+
+    def most_powerful_weapon(self):
+        max_damage = 0
+        best_weapon = None
+        for item in self.inventory:
+            try:
+                if item.damage > max_damage:
+                    best_weapon = item
+                    max_damage = item.damage
+            except AttributeError:
+                pass
+        return best_wepon
+s        
         
