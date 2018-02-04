fb8d30bdaef3ddb0d54300744638737855ed6c91
Jessica <jessica.cabrasawan@gmail.com>
2018-02-04 17:37:32 -0600

    added dev move func; init pos empty for world file

--- game.py
+++ game.py
@@ -1,17 +1,24 @@
-from player import player
+from player import Player
+import world
 
 def play():
     print("Escape from Cave Terror!")
     player = Player()
     while True:
-        action_input= get_playrt_command()
-        if action_input in ["n","N","North","north"]
+        room = world.tile_at(player.x, player.y)
+        print(room.intro_text())
+        action_input= get_player_command()
+        if action_input in ["n","N","North","north"]:
+            player.move_north()
             print("Northward Ho!")
-        elif action_input in ["s","S","South","south"]
+        elif action_input in ["s","S","South","south"]:
+            player.move_south()
             print("South, Baby!")
-        elif action_input in ["w","W","West","west"]
+        elif action_input in ["w","W","West","west"]:
+            player.move_west()
             print("West of the Word, Lets Go!")
-        elif action_input in ["e","E","East","east"]
+        elif action_input in ["e","E","East","east"]:
+            player.move_east()
             print("East.")
         elif action_input in ['i', 'I', 'inventory']:
             player.print_inventory()
