432ff08f606623b90d8c3a77f249b5863993758b
Jessica <jessica.cabrasawan@gmail.com>
2018-02-12 19:52:48 -0600

    started to integrate Folands changes

--- game.py
+++ game.py
@@ -1,13 +1,58 @@
+from random import randint 	# Used to generate random integers.
+
+from textwrap import fill	# Gives us a tool for formatting text in a much prettier fashion.
+from textwrap import dedent
+
+from terminalsize import get_terminal_size		# Allows us to determine terminal window size on any OS.
+												# Adapted for Python 3.x from https://gist.github.com/jtriley/1108174
+
 from player import Player
-import world
+from world import World
+import parse
+
+debug_mode = True
+
+game_name = "test"
+
+help_text = "To interact with this game world, you will use a basic text-based interface. \
+Try single-word commands like 'inventory' or 'west' (or their counterpart abbreviations, 'i' or 'w', respectively \
+to get started. For more complex interactions, use commands of the format [VERB][NOUN] (e.g. 'open door', \
+or in some cases, [VERB][NOUN][OBJECT] (e.g. 'attack thief with nasty knife').\
+The game will ignore the articles 'a', 'an', and 'the' (e.g. 'open the door' is the same as 'open door.').\n\n\
+To exit the game at any time, type 'exit' or 'quit'."
+
+wrap_width = 0
+
+def get_width():
+    dimensions = get_terminal_size()
+    global wrap_width 
+    if(dimensions[0] >= 20):
+        wrap_width = dimensions[0] - 5				# Get the width of the user's window so we can wrap text.
+    else:
+        wrap_width = dimensions[0]						
+    return dimensions
+
+def clear_screen():
+    terminal = get_width()
+
+    for i in range(terminal[1]):
+        print("")									# There are fancier ways to clear a screen, but this aligns our text where we want it at the bottom of the window.
+
+def print_wrap(text):
+    get_width()
+    text = dedent(text)#.replace("\t", "")
+    print(fill(text, wrap_width))
 
 def play():
-    print("Escape from Cave Terror!")
+    clear_screen()
+    print_wrap("Welcome to %s." % game_name)
     player = Player()
+    world = World()
+    print_wrap(world.tile_at(player.x,player.y).intro_text())
     while True:
-        room = world.tile_at(player.x, player.y)
-        print(room.intro_text())
-        action_input= get_player_command()
+        print("")							# Print a blank line for spacing purposes.
+        [raw_input, parsed_input] = parser.get_command()
+        print("")
         if action_input in ["n","N","North","north"]:
             player.move_north()
             print("Northward Ho!")
--- world.py
+++ world.py
@@ -6,7 +6,78 @@ class MapTile:
     def intro_text(self):
         raise NotImplementedError("Create a sublass instead!")
 
-class StartFile(MapTitle):
+class StartTile(MapTile):
     def intro_text(self):
-        return ""
+        return "Welcome! Please choose one item to continue!"
+
+class World:
+    map = [
+        ]
         
+    def __init__(self):
+        for i in range(len(self.map)):
+            for j in range(len(self.map[i])):
+                if(self.map[i][j]):
+                    self.map[i][j].x = j
+                    self.map[i][j].y = i
+					
+    def tile_at(self, x, y):
+        if x < 0 or y < 0:
+            return None
+        try:
+            return self.map[y][x]
+        except IndexError:
+            return None
+			
+    def check_north(self, x, y):
+        if y-1 < 0:
+    	    room = None
+        try:
+            room = self.map[y-1][x]
+        except IndexError:
+    	    room = None
+		
+        if(room):
+            return [True, "You head to the north."]
+        else:
+            return [False, "There doesn't seem to be anything to the north."]
+			
+    def check_south(self, x, y):
+        if y+1 < 0:
+            room = None
+        try:
+            room = self.map[y+1][x]
+        except IndexError:
+            room = None
+    
+        if(room):
+            return [True, "You head to the south."]
+        else:
+            return [False, "There doesn't seem to be anything to the south."]
+
+    def check_west(self, x, y):
+        if x-1 < 0:
+            room = None
+        try:
+            room = self.map[y][x-1]
+        except IndexError:
+            room = None
+
+        if(room):
+            return [True, "You head to the west."]
+        else:
+            return [False, "There doesn't seem to be anything to the west."]
+
+    def check_east(self, x, y):
+        if x+1 < 0:
+            room = None
+        try:
+            room = self.map[y][x+1]
+        except IndexError:
+            room = None
+
+        if(room):
+            return [True, "You head to the east."]
+        else:
+            return [False, "There doesn't seem to be anything to the east."]
+
