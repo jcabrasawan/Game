b207e51dba333161425ac1045854255a532ff570
Jessica <jessica.cabrasawan@gmail.com>
2018-02-17 13:56:08 -0600

    improved move function

--- player.py
+++ player.py
@@ -6,18 +6,6 @@ class Player:
         self.x = 0
         self.y = 0
 
-    def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
-    def move_north(self):
-        self.move(dx = 0, dy = -1)
-    def move_south(self):
-        self.move(dx = 0, dy = 1)
-    def move_west(self):
-        self.move(dx = -1, dy = 0)
-    def move_east(self):
-        self.move(dx = 1, dy = 0)
-
     def print_inventory(self):
         print("Inventory:")
         for item in self.invventory:
@@ -36,4 +24,19 @@ class Player:
             except AttributeError:
                 pass
         return best_weapon       
-        
+    
+    def move(self, dx, dy):
+		self.x += dx
+		self.y += dy
+
+	def move_north(self):
+		self.move(dx=0, dy=-1)
+
+	def move_south(self):
+		self.move(dx=0, dy=1)
+
+	def move_east(self):
+		self.move(dx=1, dy=0)
+
+	def move_west(self):
+		self.move(dx=-1, dy=0)
